1a2400d5ba37e7b556c16f4ae7ebc2e6cc331ee7
leendrew <leendrew99@gmail.com>
2024-05-01 23:26:29 +0600

    feat: local ssl

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { readFileSync } from 'node:fs';
+import { resolve, dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => {
+  const currentWorkingDir = dirname(fileURLToPath(import.meta.url));
+
+  return {
+    plugins: [react()],
+    base: '/insurance-tma/',
+    server: {
+      port: 443,
+      host: 'insurance.internal',
+      https: {
+        cert: readFileSync(resolve(currentWorkingDir, join('.', '.cert', 'https-cert.pem'))),
+        key: readFileSync(resolve(currentWorkingDir, join('.', '.cert', 'https-key.pem'))),
+      },
+    },
+  };
+});
