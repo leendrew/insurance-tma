f853ec561e1297778ff365f8582d4d34757b1a7a
leendrew <leendrew99@gmail.com>
2024-05-02 23:41:07 +0600

    fix: tsconfig path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { readFileSync } from 'node:fs';
-import { resolve, dirname, join } from 'node:path';
+import { dirname, resolve, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -8,8 +8,14 @@ export default defineConfig(() => {
   const currentWorkingDir = dirname(fileURLToPath(import.meta.url));
 
   return {
-    plugins: [react()],
+    // for github-pages
     base: '/insurance-tma/',
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': resolve(currentWorkingDir, join('.', 'src')),
+      },
+    },
     server: {
       port: 443,
       host: 'insurance.internal',
