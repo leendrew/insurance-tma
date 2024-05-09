ec92c731b2f5b023cfed4df71b0f4782430133e6
leendrew <leendrew99@gmail.com>
2024-05-09 18:30:06 +0600

    feat: mui init

---
+++ src/components/mui/MuiRoot.tsx
@@ -0,0 +1,29 @@
+import type { PropsWithChildren } from 'react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+
+const muiConfig = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+  components: {
+    MuiStack: {
+      defaultProps: {
+        useFlexGap: true,
+      },
+    },
+  },
+});
+
+interface MuiRootProps {}
+
+export function MuiRoot({ children }: PropsWithChildren<MuiRootProps>) {
+  return (
+    <>
+      <ThemeProvider theme={muiConfig}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </>
+  );
+}
