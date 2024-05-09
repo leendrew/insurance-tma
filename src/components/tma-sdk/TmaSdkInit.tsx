01d12789cc11670d8235f82ebc172e79cf78d2a3
leendrew <leendrew99@gmail.com>
2024-05-10 00:01:26 +0600

    feat: expand app when start

--- src/components/tma-sdk/TmaSdkInit.tsx
+++ src/components/tma-sdk/TmaSdkInit.tsx
@@ -1,13 +1,14 @@
 import { useEffect } from 'react';
 import type { PropsWithChildren } from 'react';
 import { setDebug } from '@tma.js/sdk';
-import { useLaunchParams, useMiniApp } from '@tma.js/sdk-react';
+import { useMiniApp, useLaunchParams, useViewport } from '@tma.js/sdk-react';
 
 interface TmaSdkInitProps {}
 
 export function TmaSdkInit({ children }: PropsWithChildren<TmaSdkInitProps>) {
   const miniApp = useMiniApp();
   const launchParams = useLaunchParams();
+  const viewPort = useViewport();
 
   useEffect(() => {
     miniApp.ready();
@@ -17,6 +18,10 @@ export function TmaSdkInit({ children }: PropsWithChildren<TmaSdkInitProps>) {
     console.log('@', launchParams);
     setDebug(true);
   }, [launchParams]);
+
+  useEffect(() => {
+    viewPort.expand();
+  }, [viewPort]);
 
   return <>{children}</>;
 }
