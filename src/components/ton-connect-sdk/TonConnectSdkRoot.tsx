cc65858a96a46d7586be496dbcde28ad56dea997
leendrew <leendrew99@gmail.com>
2024-05-10 00:01:55 +0600

    chore: ton connect placeholder

---
+++ src/components/ton-connect-sdk/TonConnectSdkRoot.tsx
@@ -0,0 +1,7 @@
+import type { PropsWithChildren } from 'react';
+
+interface TonConnectSdkRootProps {}
+
+export function TonConnectSdkRoot({ children }: PropsWithChildren<TonConnectSdkRootProps>) {
+  return <>{children}</>;
+}
