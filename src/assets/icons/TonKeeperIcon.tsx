65249216a0d4edd75355dcba17152d7853b23434
leendrew <leendrew99@gmail.com>
2024-05-12 23:59:58 +0600

    feat: ton keeper icon

---
+++ src/assets/icons/TonKeeperIcon.tsx
@@ -0,0 +1,28 @@
+export function TonKeeperIcon() {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="none"
+        viewBox="0 0 97 96"
+      >
+        <path
+          fill="#45AEF5"
+          d="M48.5 45.333 91.167 26 48.5 93.333z"
+          opacity="0.75"
+        />
+        <path
+          fill="#45AEF5"
+          d="M48.5 45.333 5.833 26 48.5 93.333z"
+          opacity="0.5"
+        />
+        <path
+          fill="#45AEF5"
+          d="M48.5 45.333 5.833 26 48.5 6.667 91.167 26z"
+        />
+      </svg>
+    </>
+  );
+}
